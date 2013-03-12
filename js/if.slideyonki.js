d535cb7e5f6a8f39d248ff55b7517f452f3c7362
janani <janu.vu@gmail.com>
2013-03-12 15:05:37 +0530

    Signed-off-by: janani <janu.vu@gmail.com>

---
+++ js/if.slideyonki.js
@@ -0,0 +1,7 @@
+$(function() {
+                $('#ei-slider').eislideshow({
+          easing    : 'easeOutExpo',
+          titleeasing : 'easeOutExpo',
+          titlespeed  : 1200
+                });
+            });
