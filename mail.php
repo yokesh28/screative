b199363b62eee9473122fd9c58e4cdf9058fbe4b
SHILLMEDIA <SHILLMEDIA@SHILLMEDIA-PC>
2013-03-18 11:20:43 +0530

    Signed-off-by: SHILLMEDIA <SHILLMEDIA@SHILLMEDIA-PC>

--- mail.php
+++ mail.php
@@ -1,25 +1,6 @@
 <?php
 
-$header="from: $_REQUEST[name] <$_REQUEST[email]>";
 
-$mobile ="$_REQUEST[mobile]";
-
-
-
-$enquiry="$_REQUEST[enquiry]";
-
-// Enter your email address
-$to ='annamalai.cs@gmail.com';
-$send_contact=mail($to,$mobile,$message,$enquiry,$header);
-
-// Check, if message sent to your email
-// display message "We've recived your information"
-if($send_contact){
-	echo "We've recived your contact information";
-}
-else {
-	echo "ERROR";
-}
 
 
 $message = '<html><body>';
@@ -36,7 +17,26 @@ $message = '<html><body>';
 			$message .= "</body></html>";
 
 			
+			$header="from: $_REQUEST[name] <$_REQUEST[email]>";
 			
+			$mobile ="$_REQUEST[mobile]";
+			
+			
+			
+			$enquiry="$_REQUEST[enquiry]";
+			
+			// Enter your email address
+			$to ='annamalai.cs@gmail.com';
+			$send_contact=mail($to,$mobile,$message,$enquiry,$header);
+			
+			// Check, if message sent to your email
+			// display message "We've recived your information"
+			if($send_contact){
+				echo "We've recived your contact information";
+			}
+			else {
+				echo "ERROR";
+			}		
 			
 			
 			
