20dad2ee4505b787b90f1a4b37d17606ed66b642
SHILLMEDIA <SHILLMEDIA@SHILLMEDIA-PC>
2013-03-18 11:09:43 +0530

    Signed-off-by: SHILLMEDIA <SHILLMEDIA@SHILLMEDIA-PC>

--- mail.php
+++ mail.php
@@ -10,15 +10,36 @@ $enquiry="$_REQUEST[enquiry]";
 
 // Enter your email address
 $to ='annamalai.cs@gmail.com';
-$send_contact=mail($to,$mobile,$enquiry,$header);
+$send_contact=mail($to,$mobile,$message,$enquiry,$header);
 
 // Check, if message sent to your email
 // display message "We've recived your information"
-if($send_contact){
-	echo "We've recived your contact information";
-}
-else {
-	echo "ERROR";
-}
-?>
 
+
+
+$message = '<html><body>';
+			
+			$message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+			$message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . strip_tags($_REQUEST['name']) . "</td></tr>";
+			$message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_REQUEST['email']) . "</td></tr>";
+			$message .= "<tr><td><strong>Mobile:</strong> </td><td>" . strip_tags($_REQUEST['mobile']) . "</td></tr>";
+			$message .= "<tr><td><strong>Enquiry:</strong> </td><td>" . strip_tags($_REQUEST['enquiry']) . "</td></tr>";
+			
+			
+			
+			$message .= "</table>";
+			$message .= "</body></html>";
+
+			
+			
+			if($send_contact){
+				echo "We've recived your contact information";
+			}
+			else {
+				echo "ERROR";
+			}	
+			
+			
+			
+			
+			?>
