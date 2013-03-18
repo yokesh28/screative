1a6ccff8622142379b0b41c84b23f0f32ac6ac9b
SHILLMEDIA <SHILLMEDIA@SHILLMEDIA-PC>
2013-03-18 15:00:51 +0530

    Signed-off-by: SHILLMEDIA <SHILLMEDIA@SHILLMEDIA-PC>

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@ $subject = 'Received mail information';
 
 $headers = "From: " . strip_tags($_POST['email']) . "\r\n";
 $headers .= "Reply-To: ". strip_tags($_POST['email']) . "\r\n";
-$headers .= "CC: susan@example.com\r\n";
+
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
