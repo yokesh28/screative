49ac9edc1d162623e4a13a69115cc1e915b157aa
lokesh <yokesh.ganesan@gmail.com>
2013-03-27 14:57:01 +0530

    Signed-off-by: lokesh <yokesh.ganesan@gmail.com>

--- page.php
+++ page.php
@@ -778,7 +778,7 @@ return false;
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  js.src = "https://connect.facebook.net/en_US/all.js#xfbml=1";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 	</div>
