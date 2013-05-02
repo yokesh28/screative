c5ce8fc8cf6fe82a3c9d81b926f036c2fe7feacf
screative2 <screative2@ubuntu>
2013-05-02 18:29:18 +0530

    Signed-off-by: screative2 <screative2@ubuntu>

---
+++ respo/header.php
@@ -0,0 +1,30 @@
+<nav class="top-bar logo">
+		<ul class="title-area">
+			<!-- Title Area -->
+			<li class="name">
+				<h1>
+					<a href="#"><img src="images/logo2.png" height="200px"
+						width="200px"></a>
+				</h1>
+			</li>
+			<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+			<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+		</ul>
+
+		<section class="top-bar-section">
+
+
+
+			<!-- Right Nav Section -->
+			<ul class="right align">
+
+				<li><a href="index.php">HOME</a></li>
+				<li><a href="about.php">ABOUT US</a></li>
+				<li><a href="#">SERVICE</a></li>
+				<li><a href="#">PORTFOLIO</a></li>
+				<li><a href="#">CONTACT US</a></li>
+
+
+			</ul>
+		</section>
+	</nav>
--- respo/index.php
+++ respo/index.php
@@ -28,36 +28,7 @@
 	<!-- body content here -->
 	<!-- header -->
 
-	<nav class="top-bar logo">
-		<ul class="title-area">
-			<!-- Title Area -->
-			<li class="name">
-				<h1>
-					<a href="#"><img src="images/logo2.png" height="200px"
-						width="200px"></a>
-				</h1>
-			</li>
-			<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-			<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-		</ul>
-
-		<section class="top-bar-section">
-
-
-
-			<!-- Right Nav Section -->
-			<ul class="right align">
-
-				<li><a href="#">HOME</a></li>
-				<li><a href="#">ABOUT US</a></li>
-				<li><a href="#">SERVICE</a></li>
-				<li><a href="#">PORTFOLIO</a></li>
-				<li><a href="#">CONTACT US</a></li>
-
-
-			</ul>
-		</section>
-	</nav>
+	<?php  include('header.php')?>
 	<div class="row">
 		<div class="container large-12 columns">
 			<ul class="home_images da-thumbs">
@@ -272,51 +243,7 @@
 
 		</div>
 	</div>
-	<footer class="row">
-
-		<div class="large-4 columns social1">Copyright © 2011.
-			S-Creative. All Rights Reserved</div>
-		<div class="right social1">
-
-			<div class="social">
-				<div class="set">
-					<img src="images/fb.png" style="margin-bottom: 24px;">
-
-
-					<iframe
-						src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fboxedcreativity&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=35&amp;appId=267011706731973"
-						scrolling="no" frameborder="0"
-						style="border: none; overflow: hidden; width: 80px; height: 26px; padding-top: 4px; padding-left: 3px;"
-						allowTransparency="true"></iframe>
-
-
-				</div>
-				<div class="set1">
-					<img src="images/tw.png" style="margin-bottom: 10px;"> <a
-						href="https://twitter.com/boxedcreativity"
-						class="twitter-follow-button" data-show-count="false"
-						data-dnt="true">Follow @boxedcreativity</a>
-					<script>
-						!function(d, s, id) {
-							var js, fjs = d.getElementsByTagName(s)[0];
-							if (!d.getElementById(id)) {
-								js = d.createElement(s);
-								js.id = id;
-								js.src = "//platform.twitter.com/widgets.js";
-								fjs.parentNode.insertBefore(js, fjs);
-							}
-						}(document, "script", "twitter-wjs");
-					</script>
-				</div>
-
-
-
-			</div>
-
-		</div>
-
-
-	</footer>
+<?php include('footer.php')?>
 	<script>
 		document.write('<script src='
 				+ ('__proto__' in {} ? 'js/vendor/zepto' : 'js/vendor/jquery')
@@ -342,35 +269,7 @@
 		$(document).foundation();
 	</script>
 	<script type="text/javascript">
-		$('.set').mouseenter(function() {
-
-			$(".set").animate({
-				"width" : "145px",
-				opacity : 1
-			}, 300);
-		});
-		$('.set').mouseleave(function() {
-
-			$(".set").animate({
-				"width" : "28px",
-				opacity : 1
-			}, 300);
-		});
-
-		$('.set1').mouseenter(function() {
-
-			$(".set1").animate({
-				"width" : "190px",
-				opacity : 1
-			}, 300);
-		});
-		$('.set1').mouseleave(function() {
-
-			$(".set1").animate({
-				"width" : "37px",
-				opacity : 1
-			}, 300);
-		});
+	
 
 		$('.da-thumbs li.hover').each(function() {
 
