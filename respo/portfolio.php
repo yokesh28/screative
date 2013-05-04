08433fce5e5163479a8506becae36462126de532
DS <DS@ds-9571ec2d157d>
2013-05-04 12:17:26 +0530

    Signed-off-by: DS <DS@ds-9571ec2d157d>

---
+++ respo/portfolio.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width" />
+<title>Portfolio</title>
+
+<link rel="stylesheet" href="css/normalize.css" />
+<!-- If you are using CSS version, add this -->
+
+<!--port-->
+
+<link href="css/main_2.css" rel="stylesheet" type="text/css" media="screen">
+
+<link rel="stylesheet" type="text/css" href="css/work.css" />
+
+<link rel="stylesheet" href="css/foundation.css" />
+<link rel="stylesheet" href="css/foundation.min.css" />
+<link rel="stylesheet" href="css/app.css" />
+<link rel="stylesheet" href="css/style.css" />
+<script src="js/vendor/custom.modernizr.js"></script>
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+
+
+
+
+
+
+</head>
+<body>
+
+	<!-- body content here -->
+	<!-- header -->
+
+	<?php  include('header.php')?>
+	<div class="row about">
+		
+		<div class="row background_image">
+		
+			
+			
+			
+			
+			<section id="arbeid">
+
+	<div class="inner">
+  
+    
+<br>
+	<ul id="arbeid_grid" class="arbeid_grid">
+	
+<li>
+<div class="view view-tenth">
+<img src="work/thumb_01.jpg">
+<div class="mask">
+<h2>Bedre Skoler</h2>
+<p>A responsive webpage consept for bedreskoler.no. Here you could see progress on things that needed to be fixed at your local school.</p>
+<a href="bedreskoler.html" class="info">View</a>
+</div>
+</div>
+</li>
+
+<li>
+<div class="view view-tenth">
+<img src="work/thumb_02.jpg">
+<div class="mask">
+<h2>Daracha</h2>
+<p>A fully responsive page for Daracha. I also took the photos that where used on the page and at their Whiskey meet stand 2012.</p>
+<a href="#" class="info">View</a>
+</div>
+</div>
+</li>
+
+<li>
+<div class="view view-tenth">
+<img src="work/thumb_03.jpg">
+<div class="mask">
+<h2>Top Model</h2>
+<p>Vignette for the norwegian Top Model. This was made as a "storyboard" in photoshop.</p>
+<a href="#" class="info">View</a>
+</div>
+</div>
+</li>
+
+
+
+
+
+</ul>
+
+</div>
+
+  </section>
+
+
+
+			
+			
+			
+
+</div>
+	</div>
+<?php include('footer.php'); ?>
+	<script>
+		document.write('<script src='
+				+ ('__proto__' in {} ? 'js/vendor/zepto' : 'js/vendor/jquery')
+				+ '.js><\/script>')
+	</script>
+	<script src="js/foundation/foundation.js"></script>
+	<script src="js/foundation/foundation.alerts.js"></script>
+	<script src="js/foundation/foundation.clearing.js"></script>
+	<script src="js/foundation/foundation.cookie.js"></script>
+	<script src="js/foundation/foundation.dropdown.js"></script>
+	<script src="js/foundation/foundation.forms.js"></script>
+	<script src="js/foundation/foundation.joyride.js"></script>
+	<script src="js/foundation/foundation.magellan.js"></script>
+	<script src="js/foundation/foundation.orbit.js"></script>
+	<script src="js/foundation/foundation.placeholder.js"></script>
+	<script src="js/foundation/foundation.reveal.js"></script>
+	<script src="js/foundation/foundation.section.js"></script>
+	<script src="js/foundation/foundation.tooltips.js"></script>
+	<script src="js/foundation/foundation.topbar.js"></script>
+	<script>
+		$(document).foundation();
+	</script>
+
+	
+	
+	
+	
+
+
+
+
+
+</body>
+</html>
