42cd0dae674a851b10a4a898dfb939c9ad1074a2
Lokesh <yokesh.ganesan@gmail.com>
2013-05-04 14:51:50 +0530

    Signed-off-by: Lokesh <yokesh.ganesan@gmail.com>

--- respo/contact.php
+++ respo/contact.php
@@ -23,36 +23,7 @@
 	<!-- body content here -->
 	<!-- header -->
 
-	<nav class="top-bar logo">
-		<ul class="title-area">
-			<!-- Title Area -->
-			<li class="name">
-				<h1>
-					<a href="#"><img src="images/logo2.png" height="200px"
-						width="200px"></a>
-				</h1>
-			</li>
-			<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-			<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-		</ul>
-
-		<section class="top-bar-section">
-
-
-
-			<!-- Right Nav Section -->
-			<ul class="right align">
-
-				<li><a href="#">HOME</a></li>
-				<li><a href="#">ABOUT US</a></li>
-				<li><a href="#">SERVICE</a></li>
-				<li><a href="#">PORTFOLIO</a></li>
-				<li><a href="#">CONTACT US</a></li>
-
-
-			</ul>
-		</section>
-	</nav>
+<?php include "header.php" ?>
 
 
 	<div class="row contact">
