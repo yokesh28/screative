4cbff954ca0a8938e3016c8a1337d90a1da57181
janani <janu.vu@gmail.com>
2013-05-06 13:51:44 +0530

    Signed-off-by: janani <janu.vu@gmail.com>

--- respo/service.php
+++ respo/service.php
@@ -261,7 +261,7 @@ style="display: none;">
 
 
 
-
+<?php include('footer.php'); ?>
 	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 
 	<script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
