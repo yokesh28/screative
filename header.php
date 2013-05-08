9bd603c08e29e7c5e1eb3295962fbb4bf313f679
DS <DS@ds-9571ec2d157d>
2013-05-08 19:38:12 +0530

    Signed-off-by: DS <DS@ds-9571ec2d157d>

---
+++ header.php
@@ -0,0 +1,31 @@
+<nav class="top-bar logo">
+		<ul class="title-area">
+			<!-- Title Area -->
+			<li class="name">
+				<h1>
+					<a href="#"><img src="images/logo2.png" height="200px"
+						width="200px"></a>
+				</h1>
+			</li>
+			<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+			<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+		</ul>
+
+		<section class="top-bar-section">
+
+
+
+			<!-- Right Nav Section -->
+			<ul class="right align">
+
+				<li><a href="index.php">HOME</a></li>
+				<li><a href="about.php">ABOUT US</a></li>
+
+				<li><a href="service.php">SERVICE</a></li>
+				<li><a href="portfolio.php">PORTFOLIO</a></li>
+				<li><a href="contact.php">CONTACT US</a></li>
+
+
+			</ul>
+		</section>
+	</nav>
