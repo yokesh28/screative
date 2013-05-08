6e3c627f357987795264c607c15934c9ed8597fb
DS <DS@ds-9571ec2d157d>
2013-05-08 19:37:52 +0530

    Signed-off-by: DS <DS@ds-9571ec2d157d>

--- respo/js/if.slideyonki.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-                $('#ei-slider').eislideshow({
-          easing    : 'easeOutExpo',
-          titleeasing : 'easeOutExpo',
-          titlespeed  : 1200
-                });
-            });
